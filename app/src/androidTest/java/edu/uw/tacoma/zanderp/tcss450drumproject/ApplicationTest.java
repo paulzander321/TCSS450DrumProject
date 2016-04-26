4a1ebcbc7b1fdd4f7bbcc034798b3c8fc2d02ed0
paulzander321 <paulzander321@gmail.com>
2016-04-25 19:29:20 -0700

    Initial commit

---
+++ app/src/androidTest/java/edu/uw/tacoma/zanderp/tcss450drumproject/ApplicationTest.java
@@ -0,0 +1,13 @@
+package edu.uw.tacoma.zanderp.tcss450drumproject;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
