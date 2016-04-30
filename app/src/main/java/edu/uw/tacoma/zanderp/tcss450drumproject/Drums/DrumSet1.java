766c0e63eaceaafc0fcbcb5c2234fd7adc36600a
Stepan Adespya <ssa7@uw.edu>
2016-04-30 11:16:49 -0700

    Drums work

--- app/src/main/java/edu/uw/tacoma/zanderp/tcss450drumproject/Drums/DrumSet1.java
+++ app/src/main/java/edu/uw/tacoma/zanderp/tcss450drumproject/Drums/DrumSet1.java
@@ -1,23 +1,19 @@
 package edu.uw.tacoma.zanderp.tcss450drumproject.Drums;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.IOException;
 
 import edu.uw.tacoma.zanderp.tcss450drumproject.R;
 
 public class DrumSet1 extends AppCompatActivity {
-    final MediaPlayer crash = MediaPlayer.create(this, R.raw.crash);
-//    final MediaPlayer hihat = MediaPlayer.create(this, R.raw.hihat);
-//    final MediaPlayer kick = MediaPlayer.create(this, R.raw.kick1);
-//    final MediaPlayer ride = MediaPlayer.create(this, R.raw.ride);
-//    final MediaPlayer snare = MediaPlayer.create(this, R.raw.snare);
-//    final MediaPlayer tomFloor = MediaPlayer.create(this, R.raw.tom1);
-//    final MediaPlayer tom1 = MediaPlayer.create(this, R.raw.tom2);
-//    final MediaPlayer tom2 = MediaPlayer.create(this, R.raw.tomshort);
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +23,42 @@ public class DrumSet1 extends AppCompatActivity {
     }
 
     public void playCrash(View view){
-        TextView playCrash = (TextView) this.findViewById(R.id.crash);
-        playCrash.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                crash.start();
-            }
-        });
+        final MediaPlayer crash = MediaPlayer.create(this,R.raw.crash);
+        crash.start();
     }
 
+    public void playHiHat(View view){
+        final MediaPlayer hihat = MediaPlayer.create(this, R.raw.hihat);
+        hihat.start();
+    }
 
+    public void playKick(View view){
+        final MediaPlayer kick = MediaPlayer.create(this, R.raw.kick1);
+        kick.start();
+    }
+
+    public void playRide(View view){
+        final MediaPlayer ride = MediaPlayer.create(this, R.raw.ride);
+        ride.start();
+    }
+
+    public void playSnare(View view){
+        final MediaPlayer snare = MediaPlayer.create(this, R.raw.snare);
+        snare.start();
+    }
+
+    public void playFloorTom(View view){
+        final MediaPlayer tomFloor = MediaPlayer.create(this, R.raw.tom1);
+        tomFloor.start();
+    }
+
+    public void playTom1(View view){
+        final MediaPlayer tom1 = MediaPlayer.create(this, R.raw.tom2);
+        tom1.start();
+    }
+
+    public void playTom2(View view){
+        final MediaPlayer tom2 = MediaPlayer.create(this, R.raw.tomshort);
+        tom2.start();
+    }
 }
