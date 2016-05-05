c17654a4f08b929dec9e18712ee5046b13fc0745
paulzander321 <paulzander321@gmail.com>
2016-05-04 17:35:10 -0700

    Authentication working

--- app/src/main/java/edu/uw/tacoma/zanderp/tcss450drumproject/Authenticate/LoginFragment.java
+++ app/src/main/java/edu/uw/tacoma/zanderp/tcss450drumproject/Authenticate/LoginFragment.java
@@ -30,9 +30,6 @@ public class LoginFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-
-        }
     }
 
     @Override
@@ -42,7 +39,54 @@ public class LoginFragment extends Fragment {
         View v =  inflater.inflate(R.layout.fragment_login_web, container, false);
         final EditText userIdText = (EditText) v.findViewById(R.id.enter_id);
         final EditText pwdText = (EditText) v.findViewById(R.id.enter_password);
+        final EditText usrnmeText = (EditText) v.findViewById(R.id.enter_username);
         Button signInButton = (Button) v.findViewById(R.id.login);
+        Button registerButton = (Button) v.findViewById(R.id.register);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userId = userIdText.getText().toString();
+                String pwd = pwdText.getText().toString();
+                String username = usrnmeText.getText().toString();
+                if (!userId.contains("@")) {
+                    Toast.makeText(v.getContext(), "Enter a valid email address"
+                            , Toast.LENGTH_SHORT)
+                            .show();
+                    userIdText.requestFocus();
+                    return;
+                }
+                if (TextUtils.isEmpty(userId))  {
+                    Toast.makeText(v.getContext(), "Enter email address"
+                            , Toast.LENGTH_SHORT)
+                            .show();
+                    userIdText.requestFocus();
+                    return;
+                }
+                if (TextUtils.isEmpty(pwd))  {
+                    Toast.makeText(v.getContext(), "Enter password"
+                            , Toast.LENGTH_SHORT)
+                            .show();
+                    pwdText.requestFocus();
+                    return;
+                }
+                if (pwd.length() < 6) {
+                    Toast.makeText(v.getContext(), "Enter password of at least 6 characters"
+                            , Toast.LENGTH_SHORT)
+                            .show();
+                    pwdText.requestFocus();
+                    return;
+                }
+                if (TextUtils.isEmpty(username)) {
+                    Toast.makeText(v.getContext(), "Enter username"
+                            , Toast.LENGTH_SHORT)
+                            .show();
+                    usrnmeText.requestFocus();
+                    return;
+                }
+
+                ((SignInActivity) getActivity()).register(username, userId, pwd);
+            }
+        });
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -114,7 +158,8 @@ public class LoginFragment extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface LoginInteractionListener {
-        public void login(String userId, String pwd);
+        void login(String userId, String pwd);
+        void register(String username, String email, String pwd);
     }
 
 }
