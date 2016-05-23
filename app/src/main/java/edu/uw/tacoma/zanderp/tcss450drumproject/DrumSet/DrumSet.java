3c508d42e7d098abb224dce85a4517c9d0836863
Stepan Adespya <ssa7@uw.edu>
2016-05-23 09:59:57 -0700

    can now play after customizing drums or customize while playing

--- app/src/main/java/edu/uw/tacoma/zanderp/tcss450drumproject/DrumSet/DrumSet.java
+++ app/src/main/java/edu/uw/tacoma/zanderp/tcss450drumproject/DrumSet/DrumSet.java
@@ -4,14 +4,10 @@ package edu.uw.tacoma.zanderp.tcss450drumproject.DrumSet;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.SystemClock;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsoluteLayout;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -23,8 +19,6 @@ import edu.uw.tacoma.zanderp.tcss450drumproject.R;
  * A simple {@link Fragment} subclass.
  */
 public class DrumSet extends Fragment {
-
-
     public DrumSet() {
         // Required empty public constructor
     }
@@ -33,20 +27,7 @@ public class DrumSet extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_drum_set, container, false);
-        return view;
+        return inflater.inflate(R.layout.fragment_drum_set, container, false);
     }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-    }
-
 
 }
