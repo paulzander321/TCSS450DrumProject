6aadad9476ec5b18f2f775af9624e42f93df457f
Stepan Adespya <ssa7@uw.edu>
2016-05-23 10:01:48 -0700

    can now play after customizing drums or customize while playing

--- app/src/main/java/edu/uw/tacoma/zanderp/tcss450drumproject/DrumSet/DrumSet.java
+++ app/src/main/java/edu/uw/tacoma/zanderp/tcss450drumproject/DrumSet/DrumSet.java
@@ -23,19 +23,6 @@ import edu.uw.tacoma.zanderp.tcss450drumproject.R;
  * A simple {@link Fragment} subclass.
  */
 public class DrumSet extends Fragment {
-    private Button snare;
-    private Button tom1;
-    private Button tom2;
-    private Button floortom;
-    private Button bass;
-    private Button crash;
-    private Button ride;
-    private Button hihat;
-    private Button pedal;
-    protected int SNARE_X;
-    protected int SNARE_Y;
-    protected int TOM1_X;
-    protected int TOM1_Y;
 
 
     public DrumSet() {
@@ -47,15 +34,6 @@ public class DrumSet extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_drum_set, container, false);
-        snare = (Button)view.findViewById(R.id.snare);
-        tom1 = (Button)view.findViewById(R.id.Tom1);
-        tom2 = (Button)view.findViewById(R.id.Tom2);
-        floortom = (Button)view.findViewById(R.id.floor_tom);
-        bass = (Button)view.findViewById(R.id.bass);
-        crash = (Button)view.findViewById(R.id.crash);
-        ride = (Button)view.findViewById(R.id.ride);
-        hihat = (Button)view.findViewById(R.id.high_hat);
-        pedal = (Button)view.findViewById(R.id.pedal);
         return view;
     }
 
